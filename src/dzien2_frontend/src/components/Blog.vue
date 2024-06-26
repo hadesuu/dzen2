ddc8518579e899d4892ce426ac1caa6423850737
hadesuu <k.gocek222@gmail.com>
2024-06-26 19:32:04 +0200

    full day 3

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
-        <h2 class="text-color-600">Wpisy na bloga:</h2>
-
+        <h2 class="text-blue-600">Wpisy na bloga:</h2>
         <div class="w-100 flex flex-row-reverse">
-            <button @click="pobierzWpisy" class="float-right bg-blue-600 rounded text-white p-4">refresh</button>
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">refresh</button>
         </div>
-
-        <div class="grid mx-6 gap-4 my-4 p-4">
-            <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-100">
-                <p>{{wpis}}</p>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="(wpis, index) in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+                <p>id: {{ index }}</p>
+                <p>{{ wpis }}</p>
+                <button  class="bg-blue-600 rounded text-white p-4" @click="deleteWpis(index)">usun</button>
             </div>
         </div>
-        <div class="grid flex-col justify-center">
-            <input v-model="nowyBlog" class="border-blue-600 p-4" type="text">
-            <button @click="dodajWpisy" class="bg-blue-600 rounded text-white p-4">dodaj</button>
-        </div>    
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" class="border-2 border-blue-600 p-4" type="text">
+            <button  class="bg-blue-600 rounded text-white p-4" @click="dodajWpisy">dodaj</button>
+        </div>
     </div>
 </template>
 
@@ -31,13 +31,18 @@ export default {
     methods: {
         async dodajWpisy() {
             await dzien2_backend.dodaj_wpis(this.nowyBlog);
+            await this.pobierzWpisy();
+        },
+        async deleteWpis(index) {
+            await dzien2_backend.usun_wpis(index);
+            await this.pobierzWpisy();
         },
         async pobierzWpisy() {
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
     },
-    async mounted() {
-        this.pobierzWpisy();
-    },
+    async mounted(){
+        this.pobierzWpisy()
+    }
 }
 </script>

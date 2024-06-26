dd534ffbc353758f79917532cb75787f317bf943
hadesuu <k.gocek222@gmail.com>
2024-06-26 19:16:56 +0200

    frontendende

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
-        <button @click="pobierzWpisy">refresh</button>
-        siema blog!
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+        <h2 class="text-color-600">Wpisy na bloga:</h2>
+
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="float-right bg-blue-600 rounded text-white p-4">refresh</button>
+        </div>
+
+        <div class="grid mx-6 gap-4 my-4 p-4">
+            <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-100">
+                <p>{{wpis}}</p>
+            </div>
+        </div>
+        <div class="grid flex-col justify-center">
+            <input v-model="nowyBlog" class="border-blue-600 p-4" type="text">
+            <button @click="dodajWpisy" class="bg-blue-600 rounded text-white p-4">dodaj</button>
+        </div>    
     </div>
 </template>
 
@@ -26,6 +36,8 @@ export default {
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
     },
-
+    async mounted() {
+        this.pobierzWpisy();
+    },
 }
 </script>
